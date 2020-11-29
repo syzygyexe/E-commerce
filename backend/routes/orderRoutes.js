672c561c736564721e57b9bccc9e123fe7bf633b
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-11-29 19:13:43 +0900

    initial

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrders,
+  getOrders,
+} from "../controllers/orderController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Routes below hooked to /api/orders route.
+// protect middleware requires a token in order to proceed further.
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route("/myorders").get(protect, getMyOrders);
+router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
+
+export default router;
