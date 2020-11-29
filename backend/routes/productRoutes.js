92399cc7d4bf0ae156c380c4d9f3a69c2cb7e834
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-11-29 19:13:55 +0900

    initial

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Routes below hooked to /api/products route.
+// protect middleware requires a token in order to proceed to a "api/users/profile" route.
+// admin middleware is valid for req.user.isAdmin.
+
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
+
+export default router;
